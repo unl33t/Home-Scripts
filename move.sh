bd0f513fec7fbae905612e45756b35cc8777c41f
unl33t g33k <wtfwasithinking@gmail.com>
2012-01-11 11:30:26 -0500

    updating methods to use rsync istead of copy to cut down on some processing time. backup of original move.sh just in case rsync doesn't work outl.

--- move.sh
+++ move.sh
@@ -4,22 +4,28 @@
 #	movie files between drive shuffles.  0 byte movies	#
 #	are noones friend.					#
 #################################################################
-if [[ "$1" =~ "/" ]];
+if [[ "$1" =~ "/" ]]
 	then
 		echo "Only run this while IN the same dir as the files to be moved!";
-	else
-		cp -pv --remove-destination $1 $2
-		origf=$(md5sum $1 | cut -d " " -f 1)
-		newf=$(md5sum $2$1 | cut -d " " -f 1)
-		if [ "$origf" = "$newf" ]
+	elif [ "$1" ] && [ "$2" ]
+		then
+#		cp -pv --remove-destination "$1" "$2"		#comment this out when testing
+		rsync -avzhP "$1" $2"$1"
+		echo "code returned: $?"
+		#origf=$(md5sum "$1" | cut -d " " -f 1)
+		#newf=$(md5sum "$2$1" | cut -d " " -f 1)
+		#if [ $origf ] && [ "$origf" = "$newf" ]
+		if [ $? == 0 ]
 			then
-				echo "Old: $origf";
-				echo "New: $newf";
-				echo "erase the orig: rm $1";
-				rm $1;
+				#echo "Old: $origf";
+				#echo "New: $newf";
+				echo "File moved, erase the orig: rm $1";
+#				rm "$1";			#comment this out when testing
 			else
-				echo "Old: $origf"
-		                echo "New: $newf";
+				#echo "Old: $origf"
+		                #echo "New: $newf";
 		                echo "Something Went Wrong (there was probably an error before this, go look for it)";
 		fi
+	else
+		echo "Source or Dest was empty";
 fi
