0e24eb5141cfbeeeb5743e669dd518c64ca9990e
unl33t g33k <wtfwasithinking@gmail.com>
2012-09-09 21:44:06 -0400

    re-writing movie type fix with perl for better filename handling.

--- movie-scripts/typefix.sh
+++ movie-scripts/typefix.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-projpath="/media/Media/pending";		# temp holding area for files
-pendpath="/media/Media/Movies/Pending";		# final bulk resting place
+projpath="/Volumes/Video/Movies/Pending";		# temp holding area for files
+pendpath="/Volumes/Video/Movies/Pending/tmp";		# final bulk resting place
+cd $projpath
 for i in `ls *.m4v *.mov *.avi *.mp4`
 do
-	if [[ $i =~ CONVERTED ]]
+	if [[ "$i" =~ CONVERTED ]]
 		then
-			echo "********** $i already converted **********";
+			echo "********** \"$i\" already converted **********";
 		else
-			newname=$(echo $i | cut -d . -f 1);
-			if [ -a $projpath/$newname.m4v ]
+			newname="$(echo \"$i\" | cut -d . -f 1)";
+			if [ -a "$projpath/$newname.m4v" ]
 				then
-					rm $projpath/$newname.m4v
+				rm "$projpath/$newname.m4v"
 			fi
-			HandBrakeCLI -Z AppleTV 2 -i $i -o $projpath/$newname.m4v;
-			mv -v $projpath/$newname.m4v $pendpath/$newname.m4v
-			mv -v $i CONVERTED-$i
+			HandBrakeCLI -Z AppleTV 3 -i $i -o "$projpath/$newname.m4v";
+			mv -v "$projpath/$newname.m4v" "$pendpath/$newname.m4v"
+			mv -v "$i" "CONVERTED-$i"
 	fi
 done
