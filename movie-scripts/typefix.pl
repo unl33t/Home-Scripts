60a084b1eee0786a493a61ce641a7148930c9648
unl33t g33k <wtfwasithinking@gmail.com>
2012-09-16 20:13:10 -0400

    silencing handbrake's chattyness now that we know the process is what I want.

--- movie-scripts/typefix.pl
+++ movie-scripts/typefix.pl
@@ -11,6 +11,6 @@ foreach $movie(@movielisting) {
     @inprocess = split(/\./, $movie);
     print "original: $original\n";
     print "inprocess: @inprocess[0]\n";
-    print `HandBrakeCLI -Z AppleTV 3 -i "$original" -o "@inprocess[0].tmp"`;
-    print `mv -v "@inprocess[0].tmp" "@inprocess[0].m4v"
+    print `HandBrakeCLI -Z AppleTV 3 -i "$original" -o "@inprocess[0].tmp" 2>&1`;
+    print `mv -v "@inprocess[0].tmp" "@inprocess[0].m4v"`;
 }
