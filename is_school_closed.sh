4949585dda59c49272da8abccfd80b219e10347a
unl33t g33k <wtfwasithinking@gmail.com>
2014-01-03 06:14:36 -0500

    quick and ugly school closings script.

---
+++ is_school_closed.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+echo "All the closed schools(`lynx -dump http://www.timesunion.com/closings/ | grep -c "Closed"`):"
+lynx -dump http://www.timesunion.com/closings/ | grep -B 1 "Closed"
+echo ""
+echo "Closed Cohoes schools(`lynx -dump http://www.timesunion.com/closings/ | grep -c "Cohoes"`):"
+lynx -dump http://www.timesunion.com/closings/ | grep -A 1 "Cohoes"
