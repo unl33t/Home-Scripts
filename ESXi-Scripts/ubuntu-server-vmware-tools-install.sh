d1744878486e4b5a6e4084b73406c95a179134c0
unl33t <wtfwasithinking@gmail.com>
2014-02-28 06:36:36 -0500

    new file:   ESXi-Scripts/ubuntu-server-vmware-tools-install.sh

---
+++ ESXi-Scripts/ubuntu-server-vmware-tools-install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# Mount the disk
+sudo mount /dev/cdrom /media/cdrom
+# extract the installer
+tar -zxvf /media/cdrom/VMwareTools-* -C /tmp/
+# go to the install dir
+cd /tmp/vmware-tools-distrib
+# run with -d to select all defaults
+sudo ./vmware-install.pl -d
+# cleanup
+umount /media/cdrom
