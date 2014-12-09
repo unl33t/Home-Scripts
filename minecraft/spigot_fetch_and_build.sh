8ae87b626cfcde2a72f9200118770a96fc1a850a
unl33t <wtfwasithinking@gmail.com>
2014-12-09 16:46:21 -0500

    spigot fetch and build for 1.8

---
+++ minecraft/spigot_fetch_and_build.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+rm BuildTools.jar
+wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+echo "building spigot in screen session spigot_build"
+echo "sc spigot_build - to watch"
+screen -d -m -S spigot_build java -jar BuildTools.jar
+echo "Build located at:  /home/g33k/minecraft/dev/spigot/Spigot/Spigot-Server/target/spigot-xxx-xxx-snapshot.jar"
