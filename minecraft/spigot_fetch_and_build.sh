2607bfe1fe09998c191a6cd6b29471fe953e79e0
Zach Falle <wtfwasithinking@gmail.com>
2014-12-09 17:00:07 -0500

    Update spigot_fetch_and_build.sh
    updated sc to screen for non-customized systems.

--- minecraft/spigot_fetch_and_build.sh
+++ minecraft/spigot_fetch_and_build.sh
@@ -2,6 +2,6 @@
 rm BuildTools.jar
 wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 echo "building spigot in screen session spigot_build"
-echo "sc spigot_build - to watch"
+echo "screen -r spigot_build - to watch"
 screen -d -m -S spigot_build java -jar BuildTools.jar
 echo "Build located at:  /home/g33k/minecraft/dev/spigot/Spigot/Spigot-Server/target/spigot-xxx-xxx-snapshot.jar"
