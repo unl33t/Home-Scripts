a706a52f86e6bde30b67aa5e4882feec1fb57539
unl33t <wtfwasithinking@gmail.com>
2014-12-12 07:14:57 -0500

    Changed use of screen to an option.  Usage:  command.sh screen Otherwise, everthing will output right to the terminal. (Build times are generally 10min or less, so screen shouldn't be needed)

--- minecraft/spigot_fetch_and_build.sh
+++ minecraft/spigot_fetch_and_build.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
-rm BuildTools.jar
+#
+#	Usage:
+#	./spigot_fetch_and_build.sh		- To watch everything live
+#	or
+#	./spigot_fetch_and_build.sh screen	- To Build in screen, in case of flaky ssh connections
+#
+#
+rm BuildTools.jar craftbukkit*.jar spigot*.jar 
 wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-echo "building spigot in screen session spigot_build"
-echo "sc spigot_build - to watch"
-screen -d -m -S spigot_build java -jar BuildTools.jar
-echo "Build located at:  Spigot/Spigot-Server/target/spigot-xxx-xxx-snapshot.jar"
+if [ "$1" == "screen" ]
+	then
+		echo "building spigot in screen session spigot_build"
+		echo "sc spigot_build - to watch"
+		screen -d -m -S spigot_build java -jar BuildTools.jar
+	else
+		java -jar BuildTools.jar
+fi
