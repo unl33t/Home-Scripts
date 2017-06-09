f95eaea9c3a7cdb87813e3fc7d500c2bfd4e3137
unl33t g33k <wtfwasithinking@gmail.com>
2017-06-09 10:16:21 -0400

    modified:   movie-scripts/movie_sync.sh

--- movie-scripts/movie_sync.sh
+++ movie-scripts/movie_sync.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-SRC=$(ls /Users/g33k/Desktop/Archived\ Movies/Prepped/ | wc -l)
-DST=$(ls /Volumes/Videos/Movies/Pending/ | wc -l)
-until [ $SRC -eq $DST ]
-    do
-        echo $SRC
-        echo $DST
-        rsync -avzh --progress --include='*.m4v' --exclude="*" Desktop/Archived\ Movies/Prepped/ /Volumes/Videos/Movies/Pending/
-        SRC=$(ls /Users/g33k/Desktop/Archived\ Movies/Prepped/ | wc -l)
-        DST=$(ls /Volumes/Videos/Movies/Pending/ | wc -l)
-    done
+if [ -d '/Volumes/Videos/Movies/Pending' ]; then
+    echo "NAS Mounted, start moving stuff!"
+    SRC=$(ls /Users/g33k/Desktop/Archived\ Movies/Prepped/ | wc -l)
+    DST=$(ls /Volumes/Videos/Movies/Pending/ | wc -l)
+    until [ $SRC -eq $DST ]
+        do
+            echo $SRC
+            echo $DST
+            rsync -avzh --progress --include='*.m4v' --exclude="*" Desktop/Archived\ Movies/Prepped/ /Volumes/Videos/Movies/Pending/
+            SRC=$(ls /Users/g33k/Desktop/Archived\ Movies/Prepped/ | wc -l)
+            DST=$(ls /Volumes/Videos/Movies/Pending/ | wc -l)
+        done
+        echo "All done, do you have to clean up the source Directory?"
+else
+    echo "make sure nas share is mounted"
+fi
