3e5364c2700c29b73ec0eb7e8dfa6b3e558e5090
unl33t <wtfwasithinking@gmail.com>
2017-08-09 10:52:58 -0400

    modified:   fucks.sh

--- fucks.sh
+++ fucks.sh
@@ -7,9 +7,11 @@ elif [ $fucks -lt 0 ] ; then
     echo "Whoa, a negagive amount of Fucks!  RUN!"
 else
     while [ $fucks -gt 0 ] ; do
+	$rt=$RANDOM
+	let "rt %= 60"
         echo "$fucks    Fucks left, use them wisely"
         let fucks-=1
-        sleep $(( ( RANDOM % 30 )  + 1 ))
+        sleep $rt
     done
     echo "All out of fucks, oh well."
 fi
