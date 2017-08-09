cbae340a1cb7f8bdc7eac7dc7d3a513b4f164dd2
unl33t g33k <wtfwasithinking@gmail.com>
2017-08-09 11:46:37 -0400

    modified:   fucks.sh - more formatting

--- fucks.sh
+++ fucks.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-echo "Pless enter the number of Fucks available: "
+echo -n "Pless enter the number of Fucks available: "
 read fucks
+echo -e "\n"
 if [ $fucks -eq 0 ] ; then
-    echo "No fucks given"
+    echo -e "No fucks given\n"
 elif [ $fucks -lt 0 ] ; then
-    echo "Whoa, a negagive amount of Fucks!  RUN!"
+    echo -e "Whoa, a negagive amount of Fucks!  RUN!\n"
 else
     while [ $fucks -gt 0 ] ; do
         if [ ! -z "$rt" ]
         then
-            echo "  (that Fuck lasted $rt seconds!)"
+            echo -e "  (that Fuck lasted $rt seconds!)\n"
         fi
         rt=$RANDOM
 	    let "rt %= 60"
@@ -17,6 +18,6 @@ else
         let fucks-=1
         sleep $rt
     done
-    echo "  (that Fuck lasted $rt seconds!)"
+    echo -e "  (that Fuck lasted $rt seconds!)\n"
     echo "All out of fucks, oh well."
 fi
