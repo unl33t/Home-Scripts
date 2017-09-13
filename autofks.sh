569ed51b44636484cb61e192f39e073da21c8bdc
unl33t g33k <wtfwasithinking@gmail.com>
2017-09-13 19:36:04 -0400

    Made max count flexible and added some graphics.

--- autofks.sh
+++ autofks.sh
@@ -1,25 +1,57 @@
 #!/bin/bash
-fucks=$RANDOM
-let "fucks %= 100"
-let fucks-=1
-
-echo -e "Fucks alotted:    $fucks\n"
-if [ $fucks -eq 0 ] ; then
-    echo -e "No fucks given\n"
-elif [ $fucks -lt 0 ] ; then
-    echo -e "Whoa, a negagive amount of Fucks!  RUN!\n"
+echo "Welcome to"
+echo "FFFFFFFFFFFFFFFFFFFFFFKKKKKKKKK    KKKKKKK   SSSSSSSSSSSSSSS "
+echo "F::::::::::::::::::::FK:::::::K    K:::::K SS:::::::::::::::S"
+echo "F::::::::::::::::::::FK:::::::K    K:::::KS:::::SSSSSS::::::S"
+echo "FF::::::FFFFFFFFF::::FK:::::::K   K::::::KS:::::S     SSSSSSS"
+echo "  F:::::F       FFFFFFKK::::::K  K:::::KKKS:::::S            "
+echo "  F:::::F               K:::::K K:::::K   S:::::S            "
+echo "  F::::::FFFFFFFFFF     K::::::K:::::K     S::::SSSS         "
+echo "  F:::::::::::::::F     K:::::::::::K       SS::::::SSSSS    "
+echo "  F:::::::::::::::F     K:::::::::::K         SSS::::::::SS  "
+echo "  F::::::FFFFFFFFFF     K::::::K:::::K           SSSSSS::::S "
+echo "  F:::::F               K:::::K K:::::K               S:::::S"
+echo "  F:::::F             KK::::::K  K:::::KKK            S:::::S"
+echo "FF:::::::FF           K:::::::K   K::::::KSSSSSSS     S:::::S"
+echo "F::::::::FF           K:::::::K    K:::::KS::::::SSSSSS:::::S"
+echo "F::::::::FF           K:::::::K    K:::::KS:::::::::::::::SS "
+echo "FFFFFFFFFFF           KKKKKKKKK    KKKKKKK SSSSSSSSSSSSSSS   "
+echo -e "\n"
+echo "The"
+echo "[F]ucks"
+echo "[K]eeping"
+echo "[S]ystem"
+echo -e "\n"
+maxfks="100"
+fks=$RANDOM
+let "fks %= maxfks"
+let fks-=1
+echo -e "fks alotted:    $fks\n"
+if [ $fks -eq 0 ] ; then
+    echo -e "No fks given\n"
+elif [ $fks -lt 0 ] ; then
+    echo -e "Whoa, a negagive amount of fks!  RUN!\n"
 else
-    while [ $fucks -gt 0 ] ; do
+    while [ $fks -gt 0 ] ; do
         if [ ! -z "$rt" ]
         then
             echo -e "  (that Fuck lasted $rt seconds!)\n"
         fi
-        rt=$RANDOM
-	    let "rt %= 60"
-        echo "$fucks    Fucks left, use them wisely"
-        let fucks-=1
-        sleep $rt
+        
+        drop=$RANDOM
+        let "drop %= maxfks"
+        if [ fks == drop ]
+        then
+            echo "Dropped a fks, don't care."
+            rt="0"
+        else
+            rt=$RANDOM
+            let "rt %= 60"
+            echo "$fks    fks left, use them wisely"
+            let fks-=1
+            sleep $rt
+        fi
     done
     echo -e "  (that Fuck lasted $rt seconds!)\n"
-    echo "All out of fucks, oh well."
+    echo "All out of fks, oh well."
 fi
