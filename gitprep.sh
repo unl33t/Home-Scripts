3b05505c9fceca1597f1df51d99a6b00d77b3e0b
unl33t g33k <wtfwasithinking@gmail.com>
2017-09-13 18:18:54 -0400

    modified:   gitprep.sh added pre-pull before push

--- gitprep.sh
+++ gitprep.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+git pull
+echo -n "Was anything sucked down? (y/n):   "
+read updates
+
+if [ $updates -eq y ] ; then
+    echo "Make sure nothing was overwritten!"
+    exit
+fi
+
 git commit -a
 git push
 echo "ignored files: "
