29a8088fb646caffc4589d2c725d27966756974d
unl33t <wtfwasithinking@gmail.com>
2018-07-24 10:41:24 -0400

    deleted:    dot_files/bash_profile 	new file:   dot_files/env_setup.sh 	deleted:    dot_files/fetch_vimrc.sh 	deleted:    dot_files/splitvtrc

---
+++ dot_files/env_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mv ~/.bashrc ~/.bashrc.old
+ln -s ~/Home-Scripts/dot_files/bashrc ~/.bashrc
+ln -s ~/Home-Scripts/dot_files/tmux ~/.tmux
+ln -s ~/Home-Scripts/dot_files/tmux.conf ~/.tmux.conf
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+source ~/.bashrc
--- dot_files/fetch_vimrc.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
