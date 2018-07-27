0b3c9392a13f0085a13757b12cda614f9ec0b413
unl33t <wtfwasithinking@gmail.com>
2018-07-27 16:47:33 -0400

    deleted:    ESXi-Scripts/kick_vms.expect 	deleted:    ESXi-Scripts/kick_vms.sh 	deleted:    ESXi-Scripts/ubuntu-server-vmware-tools-install.sh

--- ESXi-Scripts/kick_vms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Get ready to type the root password for fridge... A LOT"
-for i in 13 6
-do
-    if ssh root@fridge vim-cmd vmsvc/power.getstate $i | grep -i "Powered off"
-    then
-        ssh root@fridge vim-cmd vmsvc/power.on $i
-    fi
-done
