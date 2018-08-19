aff3396b4aa136f6cb1a9da8193c6f9011850f1f
unl33t g33k <wtfwasithinking@gmail.com>
2018-08-19 12:28:10 -0400

    new file:   ESXi-Scripts/ps-connect-power-on.ps1

---
+++ ESXi-Scripts/ps-connect-power-on.ps1
@@ -0,0 +1,14 @@
+
+
+Connect-VIServer fridge
+$VMList=Get-VM
+foreach($guest in $VMList){
+    $Name=$guest.Name
+    $PowerState=$guest.PowerState
+    Write-Host ("The sate of $Name is $PowerState")
+    if($PowerState -eq "PoweredOff"){
+        Write-Host("Starting $guest")
+        Start-VM $guest.Name
+        }
+}
+Disconnect-VIServer fridge
