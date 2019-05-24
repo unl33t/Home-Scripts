df9d25f246ac166220a72f36f10df2ceb87da708
unl33t <wtfwasithinking@gmail.com>
2019-05-24 16:08:17 -0400

    modified:   random-scripts/gitprep.sh

--- random-scripts/gitprep.sh
+++ random-scripts/gitprep.sh
@@ -10,8 +10,8 @@ fi
 
 git commit -a
 git push
-echo "ignored files: "
-git ls-files . --ignored --exclude-standard --others
+#echo "ignored files: "
+#git ls-files . --ignored --exclude-standard --others
 echo "untracked files: "
 git ls-files . --exclude-standard --others
 echo "all done"
