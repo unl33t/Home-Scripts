c94ec4f7f91db61b71d0ba239a5de412600e30fe
unl33t <wtfwasithinking@gmail.com>
2019-08-09 12:12:05 -0400

    Expanding and streamlining

--- ESXi-Scripts/env-stat.sh
+++ ESXi-Scripts/env-stat.sh
@@ -4,23 +4,25 @@ host="fridge"
 
 if $(nc -z $host 22); then # Port 22 reachable?
     echo "Host $host Online     :)"
+    echo "VM Status:"
+    ssh $host vim-cmd vmsvc/getallvms
+    vms=`ssh $host vim-cmd vmsvc/getallvms | cut -d " " -f 1`
+    for vm in $vms
+    do
+        if [[ $vm =~ "Vmid" ]]
+        then
+            echo " "
+        else
+            
+            status=$(ssh $host vim-cmd vmsvc/power.getstate $vm | tail -n 1)
+            echo "$vm $status"
+        fi
+    done
+    echo " "
+    echo "Storage Status:"
+    ssh $host esxcli storage nfs list
 else
     echo "Host $host Offline    :("
 fi
 
-echo "VM Status:"
-ssh fridge vim-cmd vmsvc/getallvms
-vms=`ssh fridge vim-cmd vmsvc/getallvms | cut -d " " -f 1`
-for vm in $vms
-do
-    if [[ $vm =~ "Vmid" ]]
-    then
-        echo " "
-    else
-        echo "Fetching $vm"
-        ssh fridge vim-cmd vmsvc/power.getstate $vm
-        echo " "
-    fi
-done
-echo "Storage Status:"
-ssh fridge esxcli storage nfs list
+
