f85847f923b0ef3a3e3bb5562e83f5ba151e6cf2
unl33t <wtfwasithinking@gmail.com>
2019-08-09 09:32:03 -0400

    renamed:    vm-getall.sh -> env-stat.sh

--- ESXi-Scripts/env-stat.sh
+++ ESXi-Scripts/env-stat.sh
@@ -1,14 +1,17 @@
-#! /bin/sh
+#!/bin/bash
+echo "VM Status:"
 ssh fridge vim-cmd vmsvc/getallvms
 vms=`ssh fridge vim-cmd vmsvc/getallvms | cut -d " " -f 1`
-sed 1d "$vms" > $vms
 for vm in $vms
 do
-    if [[ $vm == *"Vmid"* ]]; then
-        echo "Vmid"
+    if [[ $vm =~ "Vmid" ]]
+    then
+        echo " "
     else
         echo "Fetching $vm"
         ssh fridge vim-cmd vmsvc/power.getstate $vm
         echo " "
     fi
 done
+echo "Storage Status:"
+ssh fridge esxcli storage nfs list
