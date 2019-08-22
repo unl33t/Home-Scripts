190310a3867da883fb1ed6b2a86307b20344351a
unl33t <wtfwasithinking@gmail.com>
2019-08-22 13:54:08 -0400

    Moving more private files to another repo

--- ESXi-Scripts/ps-env-setup.ps1
+++
@@ -1,4 +0,0 @@
-#sudo snap install powershell --classic
-#Set-ExecutionPolicy -ExecutionPolicy Bypass
-Install-Module -Name VMware.PowerCLI –Scope CurrentUser
-Set-PowerCLIConfiguration -InvalidCertificateAction ignore
--- ESXi-Scripts/vm-power-on.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-ssh fridge vim-cmd vmsvc/getallvms
-echo "Which VM would you like to power on? "
-read vmnum
-ssh fridge vim-cmd vmsvc/power.on $vmnum
