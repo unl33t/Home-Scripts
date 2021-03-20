cc2a84cd2541d581a230d08a05e10ba2d47deedb
unl33t g33k <wtfwasithinking@gmail.com>
2021-03-20 12:23:31 -0400

    modified:   dot_files/zshrc - added docker exports 	modified:   random-scripts/is_school_closed.sh

--- random-scripts/is_school_closed.sh
+++ random-scripts/is_school_closed.sh
@@ -2,5 +2,5 @@
 echo "All the closed schools(`lynx -dump http://www.timesunion.com/closings/ | grep -c "Closed"`):"
 lynx -dump http://www.timesunion.com/closings/ | grep -B 1 "Closed"
 echo ""
-echo "Closed Cohoes schools(`lynx -dump http://www.timesunion.com/closings/ | grep -c "Cohoes"`):"
+echo "Closed Cohoes schools(`lynx -dump http://www.timesunion.com/closings/ | grep -c "Niskayuna"`):"
 lynx -dump http://www.timesunion.com/closings/ | grep -A 1 "Cohoes"
