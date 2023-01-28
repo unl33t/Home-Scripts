28de777947bd326bbfe5a6386cb526acdf212759
Zach Falle <wtfwasithinking@gmail.com>
2023-01-27 20:14:16 -0500

    docker install script

--- random-scripts/docker-setup-linux.sh
+++ random-scripts/docker-setup-linux.sh
@@ -1,15 +1,24 @@
+#!/bin/bash
+
 sudo apt-get remove docker docker-engine docker.io containerd runc
+sleep 5
 sudo apt-get update
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
+sleep 5
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sleep 5
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sleep 5
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
+sudo apt-get update
+sleep 5
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
+usermod -a -G docker g33k
